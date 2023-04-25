73bb31f0913173d34275a42d585782f4c4ff1437
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-04-25 21:23:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+describe('Accio Jobs website', () => {
+  it('contains a marquee tag with the text "Welcome to Acciojobs!" and an h1 tag with the text "Blog"', () => {
+    cy.visit('https://example.com'); // replace with the actual URL of the Accio Jobs website
+
+    cy.get('marquee').should('have.length', 1);
+    cy.get('marquee').should('have.text', 'Welcome to Acciojobs!');
+
+    cy.get('a[href="https://acciojob.com/blog/"] h1').should('have.text', 'Blog');
+  });
+});
